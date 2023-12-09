0906450b87f8827db6d682020ef14a814fab4676
lohikrme <exitus.rabies@gmail.com>
2023-12-10 01:16:20 +0200

    MySQL database finally works!

--- scripts/dbConnection.php
+++ scripts/dbConnection.php
@@ -1,8 +1,13 @@
+
 <?php
+
+// this code connects the data flow from web site
+// to database called "users" which i created in MySQL
+
 function openDbConnection() {
     $dsn = "mysql:host=localhost;dbname=users";
-    $username = "root";
-    $password = "";
+    $username = "Parrot";
+    $password = "Tosivahvasalasana";
     try {
         $conn = new PDO($dsn, $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- scripts/registration.php
+++ scripts/registration.php
@@ -3,18 +3,22 @@
 // here we send data inside our databases table "registrations".
 // notice that ID will be auto-incremented inside db.
 
-require_once 'dbConnection.php';
+// also notice that we have already opened connection to users db,
+// so this time we dont insert into users but into a table inside users
+// the table is called "registrations"
+
+require_once (__DIR__ . '/dbConnection.php');
 $conn = openDbConnection();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-    $name = mysqli_real_escape_string($conn, $_POST["name"]);
-    $password = mysqli_real_escape_string($conn, $_POST["password"]);
+    $name = $conn->quote($_POST["name"]);
+    $password = $conn->quote($_POST["password"]);
 
     try {
 
         // '$sql_command' variable stores the command to insert new values into the db's registration table: 
-        $sql_command = "INSERT INTO users (email, name, password) VALUES (:email, :name, :password)";
+        $sql_command = "INSERT INTO registrations (email, name, password) VALUES (:email, :name, :password)";
 
 
         $stmt = $conn -> prepare($sql_command);
@@ -31,7 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } 
     
     catch(PDOException $e) {
-        error_log("Virhe tietojen lisäämisessä: " . $e->getMessage() . "\n", 3, "errors.txt");
+        error_log("Virhe tietojen lisäämisessä: " . $e->getMessage() . "\n", 3, "errorLog.txt");
     }
 }
 
